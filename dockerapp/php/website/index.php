a4082e1347459da084f74fb2dc50d6bb445960d7
Lou Duquenoy <72021807+Lou-Duquenoy@users.noreply.github.com>
2023-01-02 21:50:00 +0100

    Add files via upload

--- dockerapp/php/website/index.php
+++ dockerapp/php/website/index.php
@@ -1,14 +1,14 @@
-<?php
-$host = 'db'; //Nom donné dans le docker-compose.yml
-$user = 'root';
-$password = '';
-$db = 'tutoseu';
-
-$conn = new mysqli($host,$user,$password,$db);
-if(!$conn) {echo "Erreur de connexion à MSSQL<br />";}
-else{
-        echo "Connexion à MSSQL ok<br />";
-        mysqli_close($conn);
-}
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <h1>Hello</h1>
+  <h2>ça marche !</h2>
+  <?php echo "Bonjour" ?>
+</body>
+</html>
